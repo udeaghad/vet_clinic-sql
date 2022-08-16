85f16008ae10029a793ee49e82412cf10ebec6f5
udeaghad <dozie.udeagha@gmail.com>
2022-08-16 01:27:49 +0100

    Create db, table & insert values

--- data.sql
+++ data.sql
@@ -1,5 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO ANIMALS VALUES(1,'Agumon','Feb 03, 2020',0,true,10.23);
+
+INSERT INTO ANIMALS VALUES(2,'Gabumon','Nov 15, 2018',2,true,8);
+
+INSERT INTO ANIMALS VALUES(3,'Pikachu','Jan 07, 2021',1,false,15.04);
+
+INSERT INTO ANIMALS VALUES(4,'Devimon','May 12, 2017',5,true,11);
+
